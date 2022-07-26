08998698ae3f4e5e89a0ac7d4dd65d1e33289079
EnayaAme <enaya13ame@gmail.com>
2022-07-26 20:45:31 +0200

    feat: solution added

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
+    n = Math.abs(n);
+    while (n % 10 === 0) {
+      n = n / 10;
+    }
+    let string = n.toString();
+    
+    return string.split("").reverse().join("");
 }
